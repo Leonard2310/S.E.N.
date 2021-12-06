fcae180b781f9446a0922205cce4cedd9d9e6cae
Ciro <cirog2000@gmail.com>
2021-12-06 23:49:28 +0100

    SEN al 06.12.21

--- Administrator.sql
+++ Administrator.sql
@@ -1,189 +1,32 @@
--- Si è ora connessi come "sen". 
-
--- Si procede innanzitutto con la creazione delle tabelle: File>Nuovo>Categorie: Livello database>Oggetti di database>Tabella.
-
--- 1) Tabella utente
-CREATE TABLE UTENTI(
-    NumeroDiTelefono NUMBER(10),
-    Nome VARCHAR2(30) NOT NULL,
-    Cognome VARCHAR2(30) NOT NULL,
-    Provincia CHAR(2),
-    Città VARCHAR2(35), -- La città con nome più lungo in Italia presenta ben 34 lettere 
-    CAP NUMBER(5),
-    Via VARCHAR2(35),
-    NumeroCivico NUMBER(5), -- Il numero civico più alto in Italia è circa 14500
-    
-    CONSTRAINT PK_UTENTI PRIMARY KEY(NumeroDiTelefono)
-);
--- Eseguendo ciascuna CREATE TABLE, nell'output screen appare "Creato table <nome_table>."
-
--- 2) Tabella segnalazione
-CREATE TABLE SEGNALAZIONI(
-    Codice NUMBER(10),
-    TipologiaEmergenza VARCHAR2(30) NOT NULL,
-    
-    CONSTRAINT PK_SEGNALAZIONI PRIMARY KEY(Codice)
-);
-
--- 3) Tabella dipartimento
-CREATE TABLE DIPARTIMENTI(
-    ID NUMBER(4),
-    NumeroEmergenza NUMBER(3) NOT NULL,
-    Tipologia VARCHAR2(25) NOT NULL,
-    NumeroOperatori NUMBER(6),
-    NumeroVeicoli NUMBER(4),
-    Provincia CHAR(2) NOT NULL,
-    Città VARCHAR2(35) NOT NULL,
-    CAP NUMBER(5) NOT NULL,
-    Via VARCHAR2(30) NOT NULL,
-    NumeroCivico NUMBER(5) NOT NULL,
-    
-    CONSTRAINT PK_DIPARTIMENTI PRIMARY KEY(ID)
-);
-    
--- 4) Tabella operatore
-CREATE TABLE OPERATORI(
-    ID NUMBER(6),
-    Nome VARCHAR2(30) NOT NULL,
-    Cognome VARCHAR2(30) NOT NULL,
-    SpecTitolo VARCHAR2(50),
-    IDDip NUMBER(4),
-    Ruolo VARCHAR2(30) NOT NULL,
-    
-    CONSTRAINT PK_OPERATORI PRIMARY KEY(ID)
-);
-
--- 5) Tabella titolo_di_studi
-CREATE TABLE TITOLIDISTUDI(
-    Specializzazione VARCHAR2(50),
-    
-    CONSTRAINT PK_TITILODISTUDI PRIMARY KEY(Specializzazione)
-);
-
--- 6) Tabella richiesta
-CREATE TABLE RICHIESTE(
-    NumTelefonicoUtente NUMBER(10),
-    CodSegnalazione NUMBER(10),
-    Provincia CHAR(2) NOT NULL,
-    Città VARCHAR2(35) NOT NULL,
-    CAP NUMBER(5),
-    Via VARCHAR2(35) NOT NULL,
-    NumeroCivico NUMBER(5),
-    --Data DATE,
-    --Ora TIME, --hh:mm:ss
-    TStamp TIMESTAMP(0) NOT NULL,       
-    
-    CONSTRAINT PK_RICHIESTE PRIMARY KEY(NumTelefonicoUtente, CodSegnalazione)
-);
-
--- 7) Tabella coinvolgimento
-CREATE TABLE COINVOLGIMENTI(
-    IDDip NUMBER(4),
-    CodSegnalazione NUMBER(10),
-    
-    CONSTRAINT PK_COINVOLGIMENTI PRIMARY KEY(IDDip, CodSegnalazione)
-);
-
- -- CREAZIONE RUOLI
- CREATE ROLE Ut;
-    GRANT CONNECT TO Ut;
-    GRANT INSERT ON ADMINISTRATOR.RICHIESTE TO Ut;
- 
- CREATE ROLE Op;
-    GRANT CONNECT TO Op;
-    GRANT SELECT, INSERT, DELETE, UPDATE ON ADMINISTRATOR.SEGNALAZIONI TO Op;
-
-
--- Creazione delle sequenze
-/* Si creano le seguenti sequenze:
- * 1- codSeg_seq per Codice di SEGNALAZIONE;
- * 2- idDip_seq per ID di DIPARTIMENTO;
- * 3- idOpe_seq per ID di OPERATORE.
+/* Inizialmente, non vi sono connessioni giÃ  create al di fuori degli utenti permanenti del DBMS, "sys" e "system".
+ * Dunque, bisogna identificarsi come "system": dopo aver fatto click su "Crea una connessione manualmente" - o eventualmente 
+ * sul '+' verde in alto a sinistra, nella sezione "Connessioni" -, si inserisce "system" presso le voci 'Nome' e 'Nome utente'
+ * e si inserisce la password definita in fase di installazione nel campo 'Password'.
+ * Per quanto riguarda i 'Dettagli', si lascia "localhost" in 'Nome host' e "1521" in 'Porta', mentre si sostituisce l' 'S_ID' 
+ * da "xe" (Oracle expression) a "orcl". 
+ * Eseguito il 'Test', qualora non siano stati riscontrati problemi, si fa click su 'Connetti' per avviare la connessione.
  */
--- Dopo ogni singola esecuzione, apparirà nell'output screen "Creato sequence <nome_sequence>."
-CREATE SEQUENCE codSeg_seq 
-    INCREMENT BY 1
-    START WITH 1000000
-    MAXVALUE 9999999999
-    NOMINVALUE
-    NOCYCLE;
-    
-CREATE SEQUENCE idDip_seq
-    INCREMENT BY 1
-    START WITH 1
-    MAXVALUE 9999
-    NOMINVALUE
-    NOCYCLE;
-    
-CREATE SEQUENCE idOpe_seq
-    INCREMENT BY 1
-    START WITH 10000
-    MAXVALUE 999999
-    NOMINVALUE
-    NOCYCLE;
-
-
--- Aggiunta delle chiavi esterne
-/* Si aggiungono le seguenti chiavi esterne:
- * 1- SpecTitolo:TITOLODISTUDI per OPERATORE;
- * 2- IDDip:DIPARTIMENTO per OPERATORE;
- * 3- NumTelefonicoUtente:UTENTE per RICHIESTA;
- * 4- CodSegnalazione:SEGNALAZIONE per RICHIESTA;
- * 5- IDDip:DIPARTIMENTO per COINVOLGIMENTO;
- * 6- CodSegnalazione per SEGNALAZIONE.
+ 
+/* Ci si ritrova dunque nel foglio di lavoro "system.sql".
+ * Qui bisogna definire l'utente - nel caso in esame "sen" - e attribuirgli dei privilegi.
  */
-ALTER TABLE OPERATORI
-    ADD CONSTRAINT FK_OPE_TIT FOREIGN KEY (SpecTitolo)
-    REFERENCES TITOLIDISTUDI(Specializzazione);
-    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di TitoloDiStudi.
-    
-ALTER TABLE OPERATORI
-    ADD CONSTRAINT FK_OPE_SEG FOREIGN KEY (IDDip)
-    REFERENCES DIPARTIMENTI(ID);
-    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Dipartimento.
-    
-ALTER TABLE RICHIESTE
-    ADD CONSTRAINT FK_RIC_UTE FOREIGN KEY (NumTelefonicoUtente)
-    REFERENCES UTENTI(NumeroDiTelefono);
-    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Utente.
-    
-ALTER TABLE RICHIESTE
-    ADD CONSTRAINT FK_RIC_SEG FOREIGN KEY (CodSegnalazione)
-    REFERENCES SEGNALAZIONI(Codice);
-    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Segnalazione.
-    
-ALTER TABLE COINVOLGIMENTI
-    ADD CONSTRAINT FK_COI_DIP FOREIGN KEY (IDDip)
-    REFERENCES DIPARTIMENTI(ID);
-    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Dipartimento.
-    
-ALTER TABLE COINVOLGIMENTI
-    ADD CONSTRAINT FK_COI_SEG FOREIGN KEY (CodSegnalazione)
-    REFERENCES SEGNALAZIONI(Codice);
-    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Segnalazione.
 
---  REVOCA PRIVILEGI
- REVOKE SELECT, INSERT, DELETE, UPDATE ON ADMINISTRATOR.SEGNALAZIONI FROM Op;
- REVOKE INSERT ON ADMINISTRATOR.RICHIESTE FROM Ut;
- 
- 
- -- RIMOZIONE RUOLI
- DROP ROLE Op;
- DROP ROLE Ut;
+-- Creazione utente "sen" con password "progetto_2021"
+CREATE USER sen IDENTIFIED BY progetto_2021;
+/* Si esegua soltanto tale istruzione di creazione utente: bisogna selezionarla con il mouse e successivamente su "Esegui 
+ * script" (F5). Nell'output screen verrÃ  mostrato "Creato user SEN."
+ */
+
+-- Assegnazione dei privilegi
+GRANT ALL PRIVILEGES TO sen;
+-- Con GRANT si assegnano i privilegi; ALL PRIVILEGES include tutti i privilegi messi a disposizione da Oracle SQL Developer.
+-- Eseguendo tale istruzione con F5, nell'output screen verrÃ  mostrato "Grant riuscito/a."
+
+/* La connessione attuale Ã¨ con "system". Bisogna innanzitutto disconnettersi tramite Connessioni>Disconnetti, per poi 
+ * effettuare la connessione con l'utente "sen".
+ * Basta nuovamente seguire la procedura di creazione di una nuova connessione: il nome Ã¨ ora "sen", la password Ã¨ ora 
+ * "progetto_2021".
+ */
 
 
--- Eliminazione delle sequenze
--- Dopo ogni singola esecuzione, apparirà nell'output screen "Sequence <nome_sequence> eliminato."
-DROP SEQUENCE codSeg_seq;
-DROP SEQUENCE idDip_seq;
-DROP SEQUENCE idOpe_seq;
 
--- Eliminazione delle tabelle
--- Dopo ogni singola esecuzione, apparirà nell'output screen "Table <nome_tabella> eliminato."
-DROP TABLE OPERATORI;
-DROP TABLE RICHIESTE;
-DROP TABLE COINVOLGIMENTI;
-DROP TABLE UTENTI;
-DROP TABLE DIPARTIMENTI;
-DROP TABLE TITOLIDISTUDI;
-DROP TABLE SEGNALAZIONI;
--- Utenti.sql
+++ Utenti.sql
@@ -1,29 +1,193 @@
-/* Inizialmente, non vi sono connessioni gi� create al di fuori degli utenti permanenti del DBMS, "sys" e "system".
- * Dunque, bisogna identificarsi come "system": dopo aver fatto click su "Crea una connessione manualmente" - o eventualmente 
- * sul '+' verde in alto a sinistra, nella sezione "Connessioni" -, si inserisce "system" presso le voci 'Nome' e 'Nome utente'
- * e si inserisce la password definita in fase di installazione nel campo 'Password'.
- * Per quanto riguarda i 'Dettagli', si lascia "localhost" in 'Nome host' e "1521" in 'Porta', mentre si sostituisce l' 'S_ID' 
- * da "xe" (Oracle expression) a "orcl". 
- * Eseguito il 'Test', qualora non siano stati riscontrati problemi, si fa click su 'Connetti' per avviare la connessione.
- */
- 
-/* Ci si ritrova dunque nel foglio di lavoro "system.sql".
- * Qui bisogna definire l'utente - nel caso in esame "sen" - e attribuirgli dei privilegi.
- */
+-- Si è ora connessi come "sen". 
 
--- Creazione utente "sen" con password "progetto_2021"
-CREATE USER ADMINISTRATOR IDENTIFIED BY progetto_2021;
-/* Si esegua soltanto tale istruzione di creazione utente: bisogna selezionarla con il mouse e successivamente su "Esegui 
- * script" (F5). Nell'output screen verr� mostrato "Creato user SEN."
- */
+-- Si procede innanzitutto con la creazione delle tabelle: File>Nuovo>Categorie: Livello database>Oggetti di database>Tabella.
 
--- Assegnazione dei privilegi
-GRANT ALL PRIVILEGES TO ADMINISTRATOR;
--- Con GRANT si assegnano i privilegi; ALL PRIVILEGES include tutti i privilegi messi a disposizione da Oracle SQL Developer.
--- Eseguendo tale istruzione con F5, nell'output screen verr� mostrato "Grant riuscito/a."
+-- 1) Tabella utenti
+CREATE TABLE UTENTI(
+    NumeroDiTelefono NUMBER(10),
+    Nome VARCHAR2(30) NOT NULL,
+    Cognome VARCHAR2(30) NOT NULL,
+    Provincia CHAR(2),
+    Città VARCHAR2(35), -- La città con nome più lungo in Italia presenta ben 34 lettere 
+    CAP NUMBER(5),
+    Via VARCHAR2(35),
+    NumeroCivico NUMBER(5), -- Il numero civico più alto in Italia è circa 14500
+    
+    CONSTRAINT PK_UTENTI PRIMARY KEY(NumeroDiTelefono)
+);
+-- Eseguendo ciascuna CREATE TABLE, nell'output screen appare "Creato table <nome_table>."
 
-/* La connessione attuale � con "system". Bisogna innanzitutto disconnettersi tramite Connessioni>Disconnetti, per poi 
- * effettuare la connessione con l'utente "sen".
- * Basta nuovamente seguire la procedura di creazione di una nuova connessione: il nome � ora "sen", la password � ora 
- * "progetto_2021".
+-- 2) Tabella segnalazioni
+CREATE TABLE SEGNALAZIONI(
+    Codice NUMBER(10),
+    TipologiaEmergenza VARCHAR2(30) NOT NULL,
+    
+    CONSTRAINT PK_SEGNALAZIONI PRIMARY KEY(Codice)
+);
+
+-- 3) Tabella dipartimenti
+CREATE TABLE DIPARTIMENTI(
+    ID NUMBER(4),
+    NumeroEmergenza NUMBER(3) NOT NULL,
+    Tipologia VARCHAR2(25) NOT NULL,
+    NumeroOperatori NUMBER(6),
+    NumeroVeicoli NUMBER(4),
+    Provincia CHAR(2) NOT NULL,
+    Città VARCHAR2(35) NOT NULL,
+    CAP NUMBER(5) NOT NULL,
+    Via VARCHAR2(35) NOT NULL,
+    NumeroCivico NUMBER(5) NOT NULL,
+    
+    CONSTRAINT PK_DIPARTIMENTI PRIMARY KEY(ID)
+);
+    
+-- 4) Tabella operatori
+CREATE TABLE OPERATORI(
+    ID NUMBER(6),
+    Nome VARCHAR2(30) NOT NULL,
+    Cognome VARCHAR2(30) NOT NULL,
+    SpecTitolo VARCHAR2(50),
+    IDDip NUMBER(4),
+    Ruolo VARCHAR2(30) NOT NULL,
+    
+    CONSTRAINT PK_OPERATORI PRIMARY KEY(ID)
+);
+
+-- 5) Tabella titoli_di_studi
+CREATE TABLE TITOLIDISTUDI(
+    Specializzazione VARCHAR2(50),
+    
+    CONSTRAINT PK_TITOLIDISTUDI PRIMARY KEY(Specializzazione)
+);
+
+-- 6) Tabella richieste
+CREATE TABLE RICHIESTE(
+    NumTelefonicoUtente NUMBER(10),
+    CodSegnalazione NUMBER(10),
+    Provincia CHAR(2) NOT NULL,
+    Città VARCHAR2(35) NOT NULL,
+    CAP NUMBER(5),
+    Via VARCHAR2(35) NOT NULL,
+    NumeroCivico NUMBER(5),
+    --Data DATE,
+    --Ora TIME, --hh:mm:ss
+    TStamp TIMESTAMP(0) NOT NULL,
+    
+    CONSTRAINT PK_RICHIESTE PRIMARY KEY(NumTelefonicoUtente, CodSegnalazione)
+);
+
+-- 7) Tabella coinvolgimenti
+CREATE TABLE COINVOLGIMENTI(
+    IDDip NUMBER(4),
+    CodSegnalazione NUMBER(10),
+    
+    CONSTRAINT PK_COINVOLGIMENTI PRIMARY KEY(IDDip, CodSegnalazione)
+);
+
+-- Definizione dei ruoli
+-- Ruolo utente
+CREATE ROLE utente;
+    GRANT CONNECT TO utente;
+    GRANT INSERT ON sen.RICHIESTE TO utente;
+-- Dopo ogni singolo GRANT, apparirà nell'output screen "Grant riuscito/a."
+    
+-- Ruolo operatore    
+CREATE ROLE operatore;
+    GRANT CONNECT TO operatore;
+    GRANT SELECT, INSERT, DELETE, UPDATE ON sen.SEGNALAZIONI TO operatore;
+
+-- Creazione delle sequenze
+/* Si creano le seguenti sequenze:
+ * 1- codSeg_seq per Codice di SEGNALAZIONI;
+ * 2- idDip_seq per ID di DIPARTIMENTI;
+ * 3- idOpe_seq per ID di OPERATORI.
  */
+-- Dopo ogni singola esecuzione, apparirà nell'output screen "Creato sequence <nome_sequence>."
+CREATE SEQUENCE codSeg_seq 
+    INCREMENT BY 1
+    START WITH 1000000
+    MAXVALUE 9999999999
+    NOMINVALUE
+    NOCYCLE;
+    
+CREATE SEQUENCE idDip_seq
+    INCREMENT BY 1
+    START WITH 1
+    MAXVALUE 9999
+    NOMINVALUE
+    NOCYCLE;
+    
+CREATE SEQUENCE idOpe_seq
+    INCREMENT BY 1
+    START WITH 10000
+    MAXVALUE 999999
+    NOMINVALUE
+    NOCYCLE;
+
+
+-- Aggiunta delle chiavi esterne
+/* Si aggiungono le seguenti chiavi esterne:
+ * 1- SpecTitolo:TITOLIDISTUDI per OPERATORI;
+ * 2- IDDip:DIPARTIMENTI per OPERATORI;
+ * 3- NumTelefonicoUtente:UTENTI per RICHIESTE;
+ * 4- CodSegnalazione:SEGNALAZIONI per RICHIESTE;
+ * 5- IDDip:DIPARTIMENTI per COINVOLGIMENTI;
+ * 6- CodSegnalazione:SEGNALAZIONI per SEGNALAZIONI.
+ */
+-- Dopo ogni singola esecuzione, apparirà nell'output screen "Table <nome_table> modificato."
+ALTER TABLE OPERATORI
+    ADD CONSTRAINT FK_OPE_TIT FOREIGN KEY (SpecTitolo)
+    REFERENCES TITOLIDISTUDI(Specializzazione);
+    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di TitoliDiStudi.
+    
+ALTER TABLE OPERATORI
+    ADD CONSTRAINT FK_OPE_SEG FOREIGN KEY (IDDip)
+    REFERENCES DIPARTIMENTI(ID);
+    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Dipartimenti.
+    
+ALTER TABLE RICHIESTE
+    ADD CONSTRAINT FK_RIC_UTE FOREIGN KEY (NumTelefonicoUtente)
+    REFERENCES UTENTI(NumeroDiTelefono);
+    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Utenti.
+    
+ALTER TABLE RICHIESTE
+    ADD CONSTRAINT FK_RIC_SEG FOREIGN KEY (CodSegnalazione)
+    REFERENCES SEGNALAZIONI(Codice);
+    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Segnalazioni.
+    
+ALTER TABLE COINVOLGIMENTI
+    ADD CONSTRAINT FK_COI_DIP FOREIGN KEY (IDDip)
+    REFERENCES DIPARTIMENTI(ID);
+    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Dipartimenti.
+    
+ALTER TABLE COINVOLGIMENTI
+    ADD CONSTRAINT FK_COI_SEG FOREIGN KEY (CodSegnalazione)
+    REFERENCES SEGNALAZIONI(Codice);
+    -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Segnalazioni.
+    
+
+-- Eliminazione delle sequenze
+-- Dopo ogni singola esecuzione, apparirà nell'output screen "Sequence <nome_sequence> eliminato."
+DROP SEQUENCE codSeg_seq;
+DROP SEQUENCE idDip_seq;
+DROP SEQUENCE idOpe_seq;
+
+-- Revocazione dei privilegi
+-- Dopo ogni singola esecuzione, apparirà nell'output screen "Revoke riuscito/a."
+REVOKE INSERT ON sen.RICHIESTE FROM utente;
+REVOKE SELECT, INSERT, DELETE, UPDATE ON sen.SEGNALAZIONI FROM operatore;
+
+-- Rimozione dei ruoli
+-- Dopo ogni singola esecuzione, apparirà nell'output screen "Role <nome_role> eliminato."
+DROP ROLE utente;
+DROP ROLE operatore;
+
+-- Eliminazione delle tabelle
+-- Dopo ogni singola esecuzione, apparirà nell'output screen "Table <nome_tabella> eliminato."
+DROP TABLE OPERATORI;
+DROP TABLE RICHIESTE;
+DROP TABLE COINVOLGIMENTI;
+DROP TABLE UTENTI;
+DROP TABLE DIPARTIMENTI;
+DROP TABLE TITOLIDISTUDI;
+DROP TABLE SEGNALAZIONI;
