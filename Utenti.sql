aa444358fad33114a0172cfa7329db037695a5dc
Leonardo Catello <leonardo.catello23@gmail.com>
2021-12-10 20:26:19 +0100

    ridimensionamento daiana

--- Utenti.sql
+++ Utenti.sql
@@ -1,4 +1,4 @@
--- Si è ora connessi come "sen". 
+-- Si è ora connessi come "sen".
 
 -- Si procede innanzitutto con la creazione delle tabelle: File>Nuovo>Categorie: Livello database>Oggetti di database>Tabella.
 
@@ -8,13 +8,15 @@ CREATE TABLE UTENTI(
     Nome VARCHAR2(30) NOT NULL,
     Cognome VARCHAR2(30) NOT NULL,
     Provincia CHAR(2),
-    Città VARCHAR2(35), -- La città con nome più lungo in Italia presenta ben 34 lettere 
+    Città VARCHAR2(35), -- La città con nome più lungo in Italia presenta ben 34 lettere
     CAP NUMBER(5),
     Via VARCHAR2(35),
     NumeroCivico NUMBER(5), -- Il numero civico più alto in Italia è circa 14500
-    
+
     CONSTRAINT PK_UTENTI PRIMARY KEY(NumeroDiTelefono)
-);
+)
+STORAGE(INITIAL 1.16MB NEXT 580kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
+
 -- Eseguendo ciascuna CREATE TABLE, nell'output screen appare "Creato table <nome_table>."
 
 -- 2) Tabella segnalazioni
@@ -70,9 +72,10 @@ CREATE TABLE UTENTI(
 CREATE TABLE SEGNALAZIONI(
     Codice NUMBER(10),
     TipologiaEmergenza VARCHAR2(30) NOT NULL,
-    
+
     CONSTRAINT PK_SEGNALAZIONI PRIMARY KEY(Codice)
-);
+)
+STORAGE(INITIAL 370kB NEXT 185kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
 
 -- 3) Tabella dipartimenti
 CREATE TABLE DIPARTIMENTI(
@@ -86,14 +89,15 @@ CREATE TABLE DIPARTIMENTI(
     CAP NUMBER(5) NOT NULL,
     Via VARCHAR2(35) NOT NULL,
     NumeroCivico NUMBER(5) NOT NULL,
-    
+
     CONSTRAINT PK_DIPARTIMENTI PRIMARY KEY(ID),
-    CONSTRAINT CK_DIPARTIMENTI CHECK(Tipologia IN 
+    CONSTRAINT CK_DIPARTIMENTI CHECK(Tipologia IN
                                         ('Vigili del Fuoco', 'Pronto Soccorso',
                                         'Polizia di Stato', 'Arma dei Carabinieri',
                                         'Guardia di Finanza', 'Esercito'))
-);
-    
+)
+STORAGE(INITIAL 10.9kB NEXT 2.73kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
+
 -- 4) Tabella operatori
 CREATE TABLE OPERATORI(
     ID NUMBER(6),
@@ -101,24 +105,27 @@ CREATE TABLE OPERATORI(
     Cognome VARCHAR2(30) NOT NULL,
     IDDip NUMBER(4) NOT NULL,
     Ruolo VARCHAR2(30) NOT NULL,
-    
+
     CONSTRAINT PK_OPERATORI PRIMARY KEY(ID)
-);
+)
+STORAGE(INITIAL 459kB NEXT 91.8kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
 
 -- 5) Tabella titoli_di_studi
 CREATE TABLE TITOLIDISTUDI(
     Specializzazione VARCHAR2(50),
-    
+
     CONSTRAINT PK_TITOLIDISTUDI PRIMARY KEY(Specializzazione)
-);
+)
+STORAGE(INITIAL 50kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
 
 -- 6) Tabella competenze
 CREATE TABLE COMPETENZE(
     IDOpe NUMBER(6),
-    SpecTitolo VARCHAR2(50),    
-    
+    SpecTitolo VARCHAR2(50),
+
     CONSTRAINT PK_COMPETENZE PRIMARY KEY(IDOpe, SpecTitolo)
-);
+)
+STORAGE(INITIAL 207kB NEXT 49.5kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
 
 -- 7) Tabella richieste
 CREATE TABLE RICHIESTE(
@@ -130,17 +137,20 @@ CREATE TABLE RICHIESTE(
     Via VARCHAR2(35) NOT NULL,
     NumeroCivico NUMBER(5),
     Data_Ora TIMESTAMP(0) NOT NULL,
-    
+
     CONSTRAINT PK_RICHIESTE PRIMARY KEY(NumTelefonicoUtente, CodSegnalazione)
-);
+)
+STORAGE(INITIAL 1.03MB NEXT 515kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
 
 -- 8) Tabella coinvolgimenti
 CREATE TABLE COINVOLGIMENTI(
     IDDip NUMBER(4),
     CodSegnalazione NUMBER(10),
-    
+
     CONSTRAINT PK_COINVOLGIMENTI PRIMARY KEY(IDDip, CodSegnalazione)
-);
+)
+STORAGE(INITIAL 110kB NEXT 55kB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
+
 
 -- 9) Tabella storico_segnalazioni
 CREATE TABLE STORICO_SEGNALAZIONI (
@@ -150,12 +160,13 @@ CREATE TABLE STORICO_SEGNALAZIONI (
     Codice NUMBER(10),
     TipologiaEmergenza VARCHAR2(30),
     Provincia CHAR(2),
-    Città VARCHAR2(35),  
+    Città VARCHAR2(35),
     CAP NUMBER(5),
     Via VARCHAR2(35),
     NumeroCivico NUMBER(5),
     Data_Ora TIMESTAMP(0)
-);
+)
+STORAGE(INITIAL 1.56MB NEXT 3.12MB MINEXTENTS 1 MAXEXTENTS 5 PCTINCREASE 5);
 
 
 -- Definizione dei ruoli
@@ -190,14 +201,14 @@ GRANT INSERT, UPDATE, DELETE ON sen.COINVOLGIMENTI TO centralinista;
  */
 -- Dopo ogni singola esecuzione, apparirà nell'output screen "Creato sequence <nome_sequence>."
 -- Sequenza per il codice della segnalazione
-CREATE SEQUENCE codSeg_seq 
+CREATE SEQUENCE codSeg_seq
     INCREMENT BY 1
     START WITH 1000000
     MINVALUE 1000000
     MAXVALUE 9999999999
     NOCYCLE;
-   
--- Sequenza per l'ID del dipartimento 
+
+-- Sequenza per l'ID del dipartimento
 CREATE SEQUENCE idDip_seq
     INCREMENT BY 1
     START WITH 1
@@ -224,37 +235,37 @@ CREATE SEQUENCE idOpe_seq
  * 6- IDOpe:OPERATORI per COMPETENZE;
  * 7- SpecTitolo:TITOLIDISTUDI per COMPETENZE.
  */
--- Dopo ogni singola esecuzione, apparirà nell'output screen "Table <nome_table> modificato."    
+-- Dopo ogni singola esecuzione, apparirà nell'output screen "Table <nome_table> modificato."
 ALTER TABLE OPERATORI
     ADD CONSTRAINT FK_OPE_SEG FOREIGN KEY (IDDip)
     REFERENCES DIPARTIMENTI(ID);
     -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Dipartimenti.
-    
+
 ALTER TABLE RICHIESTE
     ADD CONSTRAINT FK_RIC_UTE FOREIGN KEY (NumTelefonicoUtente)
     REFERENCES UTENTI(NumeroDiTelefono);
     -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Utenti.
-    
+
 ALTER TABLE RICHIESTE
     ADD CONSTRAINT FK_RIC_SEG FOREIGN KEY (CodSegnalazione)
     REFERENCES SEGNALAZIONI(Codice);
     -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Segnalazioni.
-    
+
 ALTER TABLE COINVOLGIMENTI
     ADD CONSTRAINT FK_COI_DIP FOREIGN KEY (IDDip)
     REFERENCES DIPARTIMENTI(ID);
     -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Dipartimenti.
-    
+
 ALTER TABLE COINVOLGIMENTI
     ADD CONSTRAINT FK_COI_SEG FOREIGN KEY (CodSegnalazione)
     REFERENCES SEGNALAZIONI(Codice);
     -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Segnalazioni.
-    
+
 ALTER TABLE COMPETENZE
     ADD CONSTRAINT FK_COM_OPE FOREIGN KEY (IDOpe)
     REFERENCES OPERATORI(ID);
     -- Nessuna operazione ON DELETE: la FK si riferisce alla chiave primaria di Operatori.
-    
+
 ALTER TABLE COMPETENZE
     ADD CONSTRAINT FK_COM_TIT FOREIGN KEY (SpecTitolo)
     REFERENCES TITOLIDISTUDI(Specializzazione);
@@ -312,14 +323,14 @@ DROP ROLE centralinista;
 
 -- Eliminazione dei vincoli di integrità referenziale
 -- Dopo ogni singola esecuzione, apparirà nell'output screen "Table <nome_tabella> modificato."
-ALTER TABLE OPERATORI      DROP CONSTRAINT FK_OPE_SEG;  
-ALTER TABLE RICHIESTE      DROP CONSTRAINT FK_RIC_UTE;    
-ALTER TABLE RICHIESTE      DROP CONSTRAINT FK_RIC_SEG;    
-ALTER TABLE COINVOLGIMENTI DROP CONSTRAINT FK_COI_DIP;    
+ALTER TABLE OPERATORI      DROP CONSTRAINT FK_OPE_SEG;
+ALTER TABLE RICHIESTE      DROP CONSTRAINT FK_RIC_UTE;
+ALTER TABLE RICHIESTE      DROP CONSTRAINT FK_RIC_SEG;
+ALTER TABLE COINVOLGIMENTI DROP CONSTRAINT FK_COI_DIP;
 ALTER TABLE COINVOLGIMENTI DROP CONSTRAINT FK_COI_SEG;
 ALTER TABLE COMPETENZE     DROP CONSTRAINT FK_COM_OPE;
 ALTER TABLE COMPETENZE     DROP CONSTRAINT FK_COM_TIT;
-    
+
 -- Eliminazione delle tabelle
 -- Dopo ogni singola esecuzione, apparirà nell'output screen "Table <nome_tabella> eliminato."
 DROP TABLE OPERATORI;
@@ -335,4 +346,3 @@ DROP TABLE STORICO_SEGNALAZIONI;
 -- Eliminazione del package
 -- Dopo l'esecuzione, apparirà nell'output screen "Package PACK_SEN eliminato."
 DROP PACKAGE PACK_SEN;
-
